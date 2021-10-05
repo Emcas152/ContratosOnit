89621566edd86bef2ac5df8797a7fe765a676c34
Fernando Reyes Monroy Martínez <freyes@maxservicios.com.gt>
2021-10-05 09:12:59 -0600

    -- Se agrega los endPoints relacionados a Condominios con sus validaciones y retorno de errores

---
+++ app/Http/Controllers/EdificioController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Edificio;
+use Illuminate\Http\Request;
+
+class EdificioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Edificio  $edificio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Edificio $edificio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Edificio  $edificio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Edificio $edificio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Edificio  $edificio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Edificio $edificio)
+    {
+        //
+    }
+}
