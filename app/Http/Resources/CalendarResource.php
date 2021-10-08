77e9c51dbf500b9309228dcb0c7c0622534858d4
Fernando Reyes Monroy Martínez <freyes@maxservicios.com.gt>
2021-10-08 08:53:28 -0600

    -- Se agrega la informacion del usuario creo y apartamento

--- app/Http/Resources/CalendarResource.php
+++ app/Http/Resources/CalendarResource.php
@@ -23,6 +23,8 @@ class CalendarResource extends JsonResource
             'start' => $this->fecha_reserva,
             'end' => $this->fecha_finaliza_reserva,
             'usuario' => $this->id_usuario,
+            'usuario_nombre' => $this->usuarioCalendario !== null ? $this->usuarioCalendario->name : '',
+            'apartamento' => $this->inquilino !== null ? $this->inquilino->nombre : '',
             'extendedProps' => [
                 'calendar' => $this->id_area,
                 'description' => $this->descripcion,
--- app/Models/CalendarioAreasSociales.php
+++ app/Models/CalendarioAreasSociales.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Amenidades;
+use App\Models\User;
+use App\Models\Apartamento;
 
 class CalendarioAreasSociales extends Model
 {
@@ -27,6 +29,16 @@ class CalendarioAreasSociales extends Model
         'hora_finaliza',
         'estado'
     ];
+
+    public function usuarioCalendario()
+    {
+        return $this->belongsTo(User::class,'id_usuario');
+    }
+
+    public function inquilino()
+    {
+        return $this->belongsTo(Apartamento::class,'id_usuario', 'id_inquilino');
+    }
 
     public function Amenidades()
     {
