b4348ba8f346a423b936620e0f5ffd12b6ae294b
Fernando Reyes Monroy Martínez <freyes@maxservicios.com.gt>
2022-01-12 10:26:45 -0600

    -- Se agrega la funcionalidad de cargar imagenes en los accesorios.

--- routes/api.php
+++ routes/api.php
@@ -104,7 +104,7 @@ Route::middleware(['auth:api'])->group(function ()
     Route::post('/accessories', [AccesorioController::class, 'index'])->name('accessories.index');
     Route::post('/accessories/select', [AccesorioController::class, 'show'])->name('accessories.show');
     Route::post('/accessories/create', [AccesorioController::class, 'store'])->name('accessories.store');
-    Route::put('/accessories/edit/{id}', [AccesorioController::class, 'update'])->name('accessories.update');
+    Route::post('/accessories/edit/{id}', [AccesorioController::class, 'update'])->name('accessories.update');
     Route::delete('/accessories/delete/{id}', [AccesorioController::class, 'destroy'])->name('accessories.destroy');
 
     Route::post('/amenities', [AmenidadController::class, 'index'])->name('amenities.index');
