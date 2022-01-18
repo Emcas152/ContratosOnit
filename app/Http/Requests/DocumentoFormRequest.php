b6577f9066d1e29a6fa43f30de420f7d8712ce57
Fernando Reyes Monroy Martínez <freyes@maxservicios.com.gt>
2022-01-18 09:40:58 -0600

    -- Se agrega las API de los documentos.

---
+++ app/Http/Requests/DocumentoFormRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class DocumentoFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id_condominio' => 'required|integer',
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'estado' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'id_condominio.required' => 'El campo condominio es obligatorio',
+            'id_condominio.integer' => 'El campo condominio debe de ser un numero entero',
+            'nombre.required' => 'El campo :attribute es obligatorio',
+            'descripcion.required' => 'El campo :attribute es obligatorio',
+            'estado.required' => 'El campo :attribute es obligatorio'
+        ];
+    }
+
+    /**
+     * Return validation errors as json response
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $response = [
+            'status' => 'failure',
+            'status_code' => 400,
+            'message' => 'Bad Request',
+            'errors' => $validator->errors(),
+        ];
+
+        throw new HttpResponseException(response()->json($response, 400));
+    }
+}
